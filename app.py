f2605031f148e14ba3b79c3dc6f65e3ef0453e9d
ShinningstarIwalewa <sofiyyahakolade72@gmail.com>
2024-07-21 20:09:56 +0100

    Finalized the wep app for predicting house pricing in Boston

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import pandas as pd
 
 app=Flask(__name__)
 ## Load the model
-regmodel=pickle.load(open('regmodel.pkl','rb'))
+regression_model=pickle.load(open('regression_model.pkl','rb'))
 scalar=pickle.load(open('scaling.pkl','rb'))
 @app.route('/')
 def home():
@@ -19,7 +19,7 @@ def predict_api():
     print(data)
     print(np.array(list(data.values())).reshape(1,-1))
     new_data=scalar.transform(np.array(list(data.values())).reshape(1,-1))
-    output=regmodel.predict(new_data)
+    output=regression_model.predict(new_data)
     print(output[0])
     return jsonify(output[0])
 
@@ -28,7 +28,7 @@ def predict():
     data=[float(x) for x in request.form.values()]
     final_input=scalar.transform(np.array(data).reshape(1,-1))
     print(final_input)
-    output=regmodel.predict(final_input)[0]
+    output=regression_model.predict(final_input)[0]
     return render_template("home.html",prediction_text="The House price prediction is {}".format(output))
 
 
